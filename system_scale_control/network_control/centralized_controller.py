6737b5041cd7640af37b9ca8dba47afdbe4e585b
Abhiram Mullapudi <pluto@Abhirams-MacBook-Pro.local>
2017-09-01 23:16:55 -0400

    Added centralized controller with unit testes (all passed)

--- system_scale_control/network_control/centralized_controller.py
+++ system_scale_control/network_control/centralized_controller.py
@@ -1,17 +1,12 @@
 import numpy as np
-import matplotlib.pylab as plt
 from pond_net import replay_memory_agent, deep_q_agent, epsi_greedy
-import random
-from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import RMSprop
 import itertools
-# Import swmm
 import sys
 sys.path.append("../../epa_swmm/")
 import swmm
-import pdb
 
 # Simulation parameters
 epi_start = float(sys.argv[1])
@@ -33,7 +28,7 @@ def build_network(input_states,
     model.add(Dense(nuron_count, input_dim=input_states))
     model.add(Activation(activation_function))
     model.add(Dropout(dropout))
-    for i_layers in range(0, hidden_layers-1):
+    for i_layers in range(0, hidden_layers - 1):
         model.add(Dense(nuron_count))
         model.add(Activation(activation_function))
         model.add(Dropout(dropout))
@@ -44,28 +39,34 @@ def build_network(input_states,
     return model
 
 # Reward Function
+
+
 def reward_function(overflow, outflow):
     """Reward Function Test"""
-    reward_out = -1.0*(outflow-0.15)*(outflow-0.05)*1000.0
+    reward_out = -1.0 * (outflow - 0.15) * (outflow - 0.05) * 1000.0
     return reward_out
+
 
 class gate_positions():
     """
     Maintains the gate position of the central controller
     """
+
     def __init__(self, nodes_list, closing_factor):
 
-        self.current_gate = np.ones((len(nodes_list))) # All gates are open
-        self.closing_factor = closing_factor # Float value < 1.0
+        self.current_gate = np.ones((len(nodes_list)))  # All gates are open
+        self.closing_factor = closing_factor  # Float value < 1.0
         self.nodes_list = nodes_list
 
     def update_gates(self, decisions):
         # Update the previous gate positions
-        self.current_gate = self.current_gate + self.closing_factor*decisions
+        self.current_gate = self.current_gate + self.closing_factor * decisions
         # Check for -ve gates
-        self.current_gate = np.maximum(np.zeros((len(self.nodes_list))),self.current_gate)
+        self.current_gate = np.maximum(
+            np.zeros((len(self.nodes_list))), self.current_gate)
         # Check for >1 gates
-        self.current_gate = np.minimum(np.ones((len(self.nodes_list))), self.current_gate)
+        self.current_gate = np.minimum(
+            np.ones((len(self.nodes_list))), self.current_gate)
 
 
 def implement_action(gate_settings, nodes, NODES_LIS):
@@ -75,18 +76,19 @@ def implement_action(gate_settings, nodes, NODES_LIS):
     for i in nodes:
         swmm.modify_setting(NODES_LIS[i], gate_settings[nodes.index(i)])
 
+
 # Nodes
-NODES_LIS = {'93-49743' : 'OR39',
-             '93-49868' : 'OR34',
-             '93-49919' : 'OR44',
-             '93-49921' : 'OR45',
-             '93-50074' : 'OR38',
-             '93-50076' : 'OR46',
-             '93-50077' : 'OR48',
-             '93-50081' : 'OR47',
-             '93-50225' : 'OR36',
-             '93-90357' : 'OR43',
-             '93-90358' : 'OR35'}
+NODES_LIS = {'93-49743': 'OR39',
+             '93-49868': 'OR34',
+             '93-49919': 'OR44',
+             '93-49921': 'OR45',
+             '93-50074': 'OR38',
+             '93-50076': 'OR46',
+             '93-50077': 'OR48',
+             '93-50081': 'OR47',
+             '93-50225': 'OR36',
+             '93-90357': 'OR43',
+             '93-90358': 'OR35'}
 
 # list of gates
 nodes_list = [i for i in NODES_LIS.keys()]
@@ -106,7 +108,8 @@ temp_gate = gates.current_gate
 input_states = np.append(temp_height, temp_gate)
 
 # Initialize Action value function
-model = target = build_network(len(input_states), 2**(len(con_ponds)) , 2, 100, 'relu', 0.0)
+model = target = build_network(
+    len(input_states), 2**(len(con_ponds)), 2, 100, 'relu', 0.0)
 
 
 if load_model_name != "i":
@@ -114,25 +117,26 @@ if load_model_name != "i":
     target.set_weights(model.get_weights())
 
 # Allocate actions
-temp_acts = itertools.product(range(len(con_ponds)), repeat=len(con_ponds))
+temp_acts = itertools.product(range(2), repeat=len(con_ponds))
 temp_acts = list(temp_acts)
-action_space = np.asarray([[-1 if j == 0 else 1 for j in i] for i in temp_acts])
-
+action_space = np.asarray([[-1 if j == 0 else 1 for j in i]
+                           for i in temp_acts])
 # Replay Memory
 replay = replay_memory_agent(len(input_states), 100000)
 
 # Deep Q learning agent
-prof_x = deep_q_agent(model, target, len(input_states), replay.replay_memory, epsi_greedy)
+prof_x = deep_q_agent(
+    model,
+    target,
+    len(input_states),
+    replay.replay_memory,
+    epsi_greedy)
 
 # Simulation Time Steps
-episode_count = 190 # Increase the epsiode count
-time_sim = 30 # Update these values
-timesteps = episode_count* time_sim
-epsilon_value = np.linspace(epi_start , epi_end, episode_count+10)
-
-# Multiple Storms
-rain_duration = ['0005', '0010', '0030','0060','0120','0180','0360','0720','1080','1440']
-return_period = ['001','002','005','010','025','050','100']
+episode_count = 1  # Increase the epsiode count
+time_sim = 25000  # Update these values
+timesteps = episode_count * time_sim
+epsilon_value = np.linspace(epi_start, epi_end, episode_count + 10)
 
 # Mean Reward
 rewards_episode_tracker = []
@@ -141,7 +145,7 @@ outflow_episode_tracker = []
 episode_tracker = 0
 t_epsi = 0
 
-mean_episode_reward = -99999 # Which model to load
+mean_episode_reward = -99999  # Which model to load
 
 # Reinforcement Learning
 while episode_tracker < episode_count:
@@ -153,7 +157,7 @@ while episode_tracker < episode_count:
     t_epsi += 1
     episode_tracker += 1
 
-    inp ='aa_orifices_v3_scs_0360min_025yr.inp'
+    inp = 'aa_orifices_v3_scs_0360min_025yr.inp'
 
     # Reset episode timer
     episode_timer = 0
@@ -165,8 +169,8 @@ while episode_tracker < episode_count:
     reward_sim = []
     outflow_sim = []
 
-    print "episode number :", episode_count, "exploration :", epsilon_value[episode_tracker]
-
+    print "episode number :", episode_count
+    print "exploration :", epsilon_value[episode_tracker]
 
     while episode_timer < time_sim:
         t_epsi += 1
@@ -174,13 +178,16 @@ while episode_tracker < episode_count:
 
         # Look at whats happening
         # Heights
-        temp_height = np.asarray([swmm.get(i, swmm.DEPTH, swmm.SI) for i in nodes_list])
+        temp_height = np.asarray(
+            [swmm.get(i, swmm.DEPTH, swmm.SI) for i in nodes_list])
 
         # Gate Positions
         temp_gate = np.asarray(gates.current_gate)
 
         # Input States
-        input_states = np.append(temp_height, temp_gate).reshape(1, len(temp_height)+len(temp_gate))
+        input_states = np.append(
+            temp_height, temp_gate).reshape(
+            1, len(temp_height) + len(temp_gate))
 
         # Action
         q_values = prof_x.ac_model.predict_on_batch(input_states)
@@ -197,7 +204,8 @@ while episode_tracker < episode_count:
         swmm.run_step()
 
         # Receive the reward
-        overflow = np.asarray([swmm.get(i, swmm.FLOODING, swmm.SI) for i in nodes_list])
+        overflow = np.asarray(
+            [swmm.get(i, swmm.FLOODING, swmm.SI) for i in nodes_list])
         outflow = swmm.get('ZOF1', swmm.INFLOW, swmm.SI)
         reward = reward_function(overflow, outflow)
         reward_sim.append(reward)
@@ -205,7 +213,8 @@ while episode_tracker < episode_count:
 
         # Update replay memory
         # Heights
-        temp_new_height = np.asarray([swmm.get(i, swmm.DEPTH, swmm.SI) for i in nodes_list])
+        temp_new_height = np.asarray(
+            [swmm.get(i, swmm.DEPTH, swmm.SI) for i in nodes_list])
 
         # Gate Positions
         temp_new_gate = np.asarray(gates.current_gate)
@@ -221,7 +230,7 @@ while episode_tracker < episode_count:
         # Train
         if episode_timer % 200 == 0:
             update = False
-            if t_epsi% 100000 == 0:
+            if t_epsi % 100000 == 0:
                 update = True
             prof_x.train_q(update)
 
@@ -229,11 +238,11 @@ while episode_tracker < episode_count:
     rewards_episode_tracker.append(np.mean(np.asarray(reward_sim)))
     outflow_episode_tracker.append(np.mean(np.asarray(outflow_sim)))
 
-    print mean_episode_reward
-    if mean_episode_reward <= rewards_episode_tracker[len(rewards_episode_tracker)-1]:
-        mean_episode_reward = rewards_episode_tracker[len(rewards_episode_tracker)-1]
+    if mean_episode_reward <= rewards_episode_tracker[len(
+            rewards_episode_tracker) - 1]:
+        mean_episode_reward = rewards_episode_tracker[len(
+            rewards_episode_tracker) - 1]
         model.save(save_model_name)
-        print "model rewritten"
 
-np.save(save_model_name+"_rewards", rewards_episode_tracker)
-np.save(save_model_name+"_outflow", outflow_episode_tracker)
+np.save(save_model_name + "_rewards", rewards_episode_tracker)
+np.save(save_model_name + "_outflow", outflow_episode_tracker)
